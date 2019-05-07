c0d46de7e9295b1f952c0739ef1fa533a7cbef46
elisaaivarone <lisa.sandrade@yahoo.com.br>
2019-05-07 13:59:02 -0300

    Projeto finalizado publicando no npm

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,27 +1,34 @@
 const expect = require("chai").expect;
 const lib = require("../lib/index")
-const cpfValidator = lib.cpfValidator;
+//const cpfValidator = lib.cpfValidator;
 
 describe("cpfValidator",() => {
-    it ("Return valid if '32222540879'",() =>{
-        expect(cpfValidator('32222540879')).to.equal(true);
+    it ("Return valid if '32222540879'",() => {
+        expect(lib.cpfValidator(32222540879)).to.equal(true);
     });
+
     it ("Return false if < 11",() => {
-      expect(cpfValidator(11)).to.equal(false);
+      expect(lib.cpfValidator('1234567890')).to.equal(false);
+
     });
-    it ("Return false if > 11",() =>{
-        expect(cpfValidator(11)).to.equal(false);
+
+    it ("Return false if > 11",() => {
+        expect(lib.cpfValidator(123456789012)).to.equal(false);
     });
+
     it ("Return invalid if '00000000000'",() =>{
-        expect(cpfValidator("00000000000")).to.equal(false);
+        expect(lib.cpfValidator("00000000000")).to.equal(false);
     });
+
     it ("Return invalid if '11111111111'",() =>{
-        expect(cpfValidator("11111111111")).to.equal(false);
+        expect(lib.cpfValidator("11111111111")).to.equal(false);
     });
+
     it ("Return invalid if '99999999999'",() =>{
-        expect(cpfValidator("99999999999")).to.equal(false);
+        expect(lib.cpfValidator("99999999999")).to.equal(false);
     });
+
     it ("Return invalid if 'aaaaaa'",() =>{
-        expect(cpfValidator('aaaaaa')).to.equal(false);
+        expect(lib.cpfValidator('aaaaaa')).to.equal(false);
     });
 })
